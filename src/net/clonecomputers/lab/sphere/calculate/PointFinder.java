e8cb89947360036b69d4a90050ebe580fd271617
g_rocket <g.rocket1@gmail.com>
2014-05-19 21:51:11 -0400

    allow concurrantly generating a whole ton of datapoints!

--- src/net/clonecomputers/lab/sphere/calculate/PointFinder.java
+++ src/net/clonecomputers/lab/sphere/calculate/PointFinder.java
@@ -4,10 +4,14 @@ import static java.lang.Math.*;
 import static net.clonecomputers.lab.sphere.Point3D.*;
 
 import java.awt.*;
+import java.util.List;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 import javax.swing.*;
+
+import org.apache.commons.csv.*;
 
 import net.clonecomputers.lab.sphere.*;
 import net.clonecomputers.lab.sphere.graph.*;
@@ -20,8 +24,102 @@ public class PointFinder {
 	private Grapher g;
 	private PrintWriter outputCSV;
 
-	public static void main(String[] args) {
-		new PointFinder().run();
+	static volatile int x;
+	static volatile boolean shouldStop = false;
+	public static void main(String[] args) throws IOException {
+		if(args.length == 0) {
+			new PointFinder().run();
+			return;
+		}
+		System.out.println("type 'stop' to stop");
+		File saveDir = new File(System.getProperty("user.home"), "points on sphere");
+		ExecutorService exec = Executors.newFixedThreadPool(9);
+		exec.execute(new Runnable() {
+			@Override public void run() {
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+				while(true) {
+					try {
+						String s = in.readLine();
+						if(s != null && s.equalsIgnoreCase("stop")) {
+							shouldStop = true;
+							break;
+						}
+					} catch (IOException e) {
+						//if(e.getMessage().equals("Stream closed")) break; // expected (not any more)
+						throw new RuntimeException(e);
+					}
+					Thread.yield();
+				}
+			}
+		});
+		PointFinder useless = new PointFinder();
+		int i;
+		outer:
+		for(i = 1; true; i++) {
+			x++;
+			exec.execute(useless.new PointRunner(i,saveDir));
+			do {
+				if(shouldStop) break outer;
+				Thread.yield();
+			} while(x > 9);
+		}
+		System.out.println("waiting for tasks to terminate (started "+i+")");
+		//System.in.close();
+		exec.shutdown();
+	}
+	
+	private class PointRunner implements Runnable {
+		final int i;
+		final File saveDir;
+		private PointRunner(int i, File saveDir) {
+			this.i = i;
+			this.saveDir = saveDir;
+		}
+		
+		@Override public void run() {
+			try {
+				new PointFinder().run(i, saveDir);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			System.out.println(i+" done");
+			x--;
+		}
+	}
+	
+	private void run(int nPoints, File saveDir) throws IOException {
+		saveDir.mkdirs();
+		File value = new File(saveDir, nPoints+"-value.csv");
+		value.createNewFile();
+		File adjacency = new File(saveDir, nPoints+"-adjacency.csv");
+		adjacency.createNewFile();
+		
+		outputCSV = new PrintWriter(value);
+		
+		SpherePoint[] points = new SpherePoint[nPoints];
+		for(int i = 0; i < points.length; i++) {
+			points[i] = new SpherePoint(false);
+		}
+		points = optimizePoints(points);
+		List<ConnectedPoint> cPoints = RigidTester.makeConnections(Arrays.asList(points));
+		CSVPrinter csv = null;
+		try {
+			csv = new CSVPrinter(new BufferedWriter(new FileWriter(adjacency)), CSVFormat.EXCEL);
+			for(ConnectedPoint p: cPoints) {
+				for(ConnectedPoint p2: cPoints) {
+					if(p.connections.contains(p2)) {
+						csv.print(1);
+					} else {
+						csv.print(0);
+					}
+				}
+				csv.println();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			csv.close();
+		}
 	}
 
 	private void run() {
@@ -37,7 +135,7 @@ public class PointFinder {
 			}
 		}
 		
-		JFileChooser fc = new JFileChooser();
+		/*JFileChooser fc = new JFileChooser();
 		int retval = fc.showSaveDialog(null);
 		File f = fc.getSelectedFile();
 		if(f != null && !f.getName().endsWith(".csv")) f = new File(f.getAbsolutePath()+".csv");
@@ -55,14 +153,14 @@ public class PointFinder {
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-		}
-		g = new Grapher();
+		}*/
+		/*g = new Grapher();
 		JFrame gWindow = new JFrame("PointsOnSphere "+numPoints);
 		gWindow.setContentPane(g);
 		gWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gWindow.pack();
 		gWindow.setSize(600,600);
-		gWindow.setVisible(true);
+		gWindow.setVisible(true);*/
 		r = new Renderer(600,600);
 		//r.addPoint(new SpherePoint(0, -PI/2), new PointProperties(Color.GREEN, false), false);
 		//r.addPoint(new SpherePoint(0, PI/2), new PointProperties(Color.GREEN, false), false);
@@ -74,13 +172,12 @@ public class PointFinder {
 					),false);
 		}
 		r.updateDisplay();
-		SpherePoint[] optimized = optimizePoints();
+		SpherePoint[] optimized = optimizePoints(r.getAllPoints());
 		System.out.println(Arrays.toString(optimized));
 		RigidTester.isRigid(Arrays.asList(optimized));
 	}
 
-	private SpherePoint[] optimizePoints() {
-		SpherePoint[] points = r.getAllPoints();
+	private SpherePoint[] optimizePoints(SpherePoint[] points) {
 		for(int step = 20; step < 100000; step++) {
 			shuffle(points);
 			double score = -Double.MAX_VALUE;
@@ -98,16 +195,16 @@ public class PointFinder {
 				}
 				if(closestPoint != null) moveApart(p,closestPoint,1.0/step);
 			}
-			g.addToGraph(step, score);
-			outputCSV.println(score);
+			if(g != null) g.addToGraph(step, score);
+			if(outputCSV != null) outputCSV.println(score);
 			if(step%500 == 0) {
-				r.setMaxCos(score);
-				r.updateDisplay();
-				System.out.println(score);
-				g.updateChart();
+				if(r != null) r.setMaxCos(score);
+				if(r != null) r.updateDisplay();
+				if(r != null || g != null) System.out.println(score);
+				if(g != null) g.updateChart();
 			}
 		}
-		g.updateChart();
+		if(g != null) g.updateChart();
 		return points;
 	}
 
